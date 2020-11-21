5d479e762078dd9a7c30d008a4a02ac5211880ae
Javi Bono <jbonifacioh01@informatica.iesvalledeljerteplasencia.es>
2020-11-21 20:33:56 +0100

    Emtrega Basica + Documentacion

--- src/ActionBoton.java
+++ src/ActionBoton.java
@@ -2,14 +2,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * Clase que implementa el listener de los botones del Buscaminas.
- * De alguna manera tendrá que poder acceder a la ventana principal.
- * Se puede lograr pasando en el constructor la referencia a la ventana.
- * Recuerda que desde la ventana, se puede acceder a la variable de tipo ControlJuego
+ * Clase que implementa el listener de los botones del Buscaminas. De alguna
+ * manera tendrá que poder acceder a la ventana principal. Se puede lograr
+ * pasando en el constructor la referencia a la ventana. Recuerda que desde la
+ * ventana, se puede acceder a la variable de tipo ControlJuego
+ * 
  * @author Javier Bonifacio Hernández
  **
  */
-public class ActionBoton implements ActionListener{
+public class ActionBoton implements ActionListener {
 
 	private VentanaPrincipal ventana;
 	private int i;
@@ -20,19 +21,21 @@ public class ActionBoton implements ActionListener{
 		this.i = i;
 		this.j = j;
 	}
-	
+
 	/**
-	 *Acción que ocurrirá cuando pulsamos uno de los botones.
+	 * Acción que ocurrirá cuando pulsamos uno de los botones. Si la casilla no es
+	 * una mina me muestra las que hay alrededor y si en algun momento piso una mina
+	 * muestra el final del juego
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(ventana.getJuego().abrirCasilla(i, j)){
-			ventana.mostrarNumMinasAlrededor(i, j);	
-			if(ventana.getJuego().esFinJuego()){
+		if (ventana.getJuego().abrirCasilla(i, j)) {
+			ventana.mostrarNumMinasAlrededor(i, j);
+			if (ventana.getJuego().esFinJuego()) {
 				ventana.mostrarFinJuego(false);
 			}
-		}else{
-			ventana.mostrarFinJuego(true);			
-		}	
+		} else {
+			ventana.mostrarFinJuego(true);
+		}
 	}
 }
